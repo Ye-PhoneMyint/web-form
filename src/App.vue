024de9b0edd22d0951ca092b0eeaf8b07124ad6b
Ye-Phone-Myint <ypm.dev@outlook.com>
2022-07-17 12:53:36 +0630

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <SignupForm></SignupForm>
+</template>
+
+<script>
+import SignupForm from './components/SignupForm'
+
+export default {
+  name: 'App',
+  components: {
+    SignupForm,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  background: #eee;
+}
+</style>
